03e147dcc77a27d63875810068989af214e44230
YakkoRasZ14 <75607956+YakkoRasZ14@users.noreply.github.com>
2021-03-25 01:21:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("json spaces", 2);
 app.use("/", api);
 
 app.get("*", async (req, res) => {
-  res.redirect("https://www.apimau.ga");
+  res.redirect("https://yakko-api.herokuapp.com");
 });
 
 app.listen(PORT, () => {
